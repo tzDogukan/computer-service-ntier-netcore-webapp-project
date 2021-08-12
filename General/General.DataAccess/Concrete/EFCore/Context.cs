c72970dac40fc83d44dfc359a49ad6b975e542c6
Doğukan Terzi <tzdogukan@gmail.com>
2021-08-12 14:17:27 +0300

    computer-service-ntier-netcore-webapp-project

---
+++ General/General.DataAccess/Concrete/EFCore/Context.cs
@@ -0,0 +1,23 @@
+﻿using General.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.DataAccess.Concrete.EFCore
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=GeneralDb;integrated security=true;MultipleActiveResultSets=true;");
+
+
+        }
+        public DbSet<ControlCenter> ControlCenters { get; set; }
+        public virtual DbSet<CompanyService> CompanyService { get; set; }
+
+    }
+}
